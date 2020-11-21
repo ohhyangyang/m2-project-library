d3ed90762d1112a50b948f684256ea51bd56998f
Yang Ge <holayangge@gmail.com>
2020-11-21 15:55:59 +0100

    updated hashedPasword of user mock data

--- bin/users-mock-data.js
+++ bin/users-mock-data.js
@@ -2,7 +2,7 @@ const users = [
   {
     username: "bookworm",
     email: "bookworm@mail.com",
-    password: "bookworm123",
+    password: "$2b$10$0XLjOddUq21suQWLGDEJkOUUxrSk9/Oe2gkIc6MMVXtcsd7p555SC",
     description: "I like books and you?",
     imageURL:"https://ctl.s6img.com/society6/img/aV8OONX8PihNtLivwp324vArC7c/w_1500/prints/~artwork/s6-0060/a/25021301_2895423/~~/book-nerd-l3g-prints.jpg", 
     booksBorrowed:[],
@@ -11,7 +11,7 @@ const users = [
   {
     username: "readinggirl",
     email: "reading@mail.com",
-    password: "readinggirl123",
+    password: "$2b$10$D0SljGBvk4gSItgz2TvarutSEm9/TRhCE8n2iQkCLwMS4ghVxy5/K",
     description: "reading 24/7",
     imageURL:"https://www.reprodart.com/kunst/michael_peter_ancher/young-girl-reading.jpg", 
     booksBorrowed:[],
@@ -20,7 +20,7 @@ const users = [
   {
     username: "yangyang",
     email: "yangyang@gmail.com",
-    password: "yangyang123",
+    password: "$2b$10$w7v3LkBoY/CqNk42fJ9eyeDgr/iq3q6hWfE4X1yrgwCkpkvcfBijm",
     description: "Prefer drawing than reading",
     imageURL:"https://avatars2.githubusercontent.com/u/65769058?s=460&u=fb60ab6bb034aa94e64e0784bd2749dfa460ca1a&v=4", 
     booksBorrowed:[],
@@ -29,7 +29,7 @@ const users = [
   {
     username: "juliane",
     email: "juliane@mail.com",
-    password: "juliane123",
+    password: "$2b$10$Rz6yjflHfLhk0rbl99x78ejyc1.Bfayw819qzDTImrhAMwXyhdkAG",
     description: "Passionate reader",
     imageURL:"https://www.diariodesevilla.es/2019/02/17/ocio/nina-Juliane-Lepoureau-escena-pelicula_1328877559_104780784_1200x675.jpg", 
     booksBorrowed:[],
@@ -38,7 +38,7 @@ const users = [
   {
     username: "bob",
     email: "bob@mail.com",
-    password: "bob123",
+    password: "$2b$10$Boxyzh102R5wtlgDOtLUIeVVrfJ/iJCmuVaZ6QhC5mM0Z.Wyb7FjK",
     description: "Hi, I'm bob",
     imageURL:"https://los40es00.epimg.net/los40/imagenes/2020/06/15/cinetv/1592210764_816634_1592211823_miniatura_normal.jpg", 
     booksBorrowed:[],
@@ -47,7 +47,7 @@ const users = [
   {
     username: "marta",
     email: "marta@mail.com",
-    password: "marta123",
+    password: "$2b$10$z28JCp/6JqazYGf/eeYAF.BgEE6ldjInt1Zq11HWSXHOB4KdIhgdu",
     description: "I love reading at the beach",
     imageURL:"https://www.mmarti.es/wp-content/uploads/2020/11/marta_marti-15-1280x1920.jpg", 
     booksBorrowed:[],
@@ -56,7 +56,7 @@ const users = [
   {
     username: "david",
     email: "david@mail.com",
-    password: "david123",
+    password: "$2b$10$pBrNkL2tuPXn6dYwloueae811Obq8k78ytz0KoRIp/3PJwCdKowcW",
     description: "Viva la vida",
     imageURL:"https://www.quien.net/wp-content/uploads/David.jpg", 
     booksBorrowed:[],
@@ -65,7 +65,7 @@ const users = [
   {
     username: "uros",
     email: "uros@mail.com",
-    password: "uros123",
+    password: "$2b$10$ewcqNiBvHagZ1ucos9pbcuF3Dr7yN7MUo3w2jNVcHQXtNUeh9sf.q",
     description: "Recording",
     imageURL:"https://www.strath.ac.uk/media/1newwebsite/courses/science/600x600/Adv_Comp_Sci.jpg", 
     booksBorrowed:[],
@@ -75,7 +75,7 @@ const users = [
   {
     username: "anna",
     email: "anna@mail.com",
-    password: "anna123",
+    password: "$2b$10$MI6hUbN9Z5sQteCPahgnx.mMZQxQ/5qrnVuiO1EJVAa11StcTMhoq",
     description: "Reading calms me down",
     imageURL:"https://swashvillage.org/storage/img/images_3/anna-pavlova-biography.jpg", 
     booksBorrowed:[],
@@ -84,7 +84,7 @@ const users = [
   {
     username: "readingboy",
     email: "readingboy@mail.com",
-    password: "readingboy123",
+    password: "$2b$10$vsxSRDrNtIdK/t.l8ppBOeMO1E/MFRaBmlPRSqiH7Pwd/2/AzJux2",
     description: "I like reading comics!",
     imageURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Frans_Hals_023.jpg/849px-Frans_Hals_023.jpg", 
     booksBorrowed:[],
