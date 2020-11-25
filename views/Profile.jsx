344dbed8245b34258c8ac418fb82a2dbdfac321f
Juliane <juliane.dominique.huber@gmail.com>
2020-11-25 12:48:09 +0100

    Change css style for profile view

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -4,55 +4,94 @@ const Layout = require("./Layout");
 
 function Profile(props) {
   return (
-    <Layout title="Profile" isLoggedIn={props.userIsLoggedIn} username={props.username} >
+    <Layout
+      title="Profile"
+      isLoggedIn={props.userIsLoggedIn}
+      username={props.username}
+    >
       <div id="profileSection">
-  
-        <img id="profilepicture" src={props.visitedUser.imageURL?(props.visitedUser.imageURL):(props.session.imageURL)}/>
-        <h3>{props.visitedUser.username?(props.visitedUser.username):(props.session.username)}</h3>
-        <p>
+        <div>
+          <img
+            id="profilepicture"
+            src={
+              props.visitedUser.imageURL
+                ? props.visitedUser.imageURL
+                : props.session.imageURL
+            }
+          />
+          {/*{props.visitedUser.username?(props.visitedUser.username):(props.session.username)}*/}
+        </div>
+        <div>
           {props.userLibrary.length <= 1
             ? `${props.userLibrary.length} book`
             : `${props.userLibrary.length} books`}
-        </p>
-        <p>
-        {props.borrowedLibrary.length <= 1
-            ? `${props.borrowedLibrary.length} borrowed book`
-            : `${props.borrowedLibrary.length} borrowed books`}  
-        </p>
-        <p>{props.visitedUser.username?(props.visitedUser.username):(props.session.username)}</p>
-        <p>{props.visitedUser.description ? props.visitedUser.description : props.session.description}</p>
-        
-        <a href="/private/edit-profile">{(!props.visitedUser.imageURL)?"EDIT PROFILE":null}</a>
-        <br />
-        <a href="/books/add">{(!props.visitedUser.imageURL)?"ADD BOOK":null}</a>
-      </div>
-       
-        <div id="approvedBookRequests">
-          <p>{props.messagesUnseen && props.messagesUnseen.length >= 1 ? "The following books have been approved for borrowing" : null}</p>
-          {props.messagesUnseen
-            ? props.messagesUnseen.map((message, i) => {
-                return (
-                  <li key={i}>
-                    {message.content}
-                    <a href={`/private/profile/message/${message._id}/${i}`}>
-                      I got it
-                    </a>
-                  </li>
-                );
-              })
-            : null}
         </div>
+        <div>
+          {props.borrowedLibrary.length <= 1
+            ? `${props.borrowedLibrary.length} borrowed book`
+            : `${props.borrowedLibrary.length} borrowed books`}
+        </div>
+      </div>
+      <div id="profileInfo">
+        <p>
+          {props.visitedUser.username
+            ? props.visitedUser.username
+            : props.session.username}{" "}
+        </p>{" "}
+        <p>
+          📚📚📚
+          {props.visitedUser.description
+            ? props.visitedUser.description
+            : props.session.description}
+        </p>
+      </div>
+      <div id="profilebuttons">
+        <a href="/private/edit-profile" class="btn btn-info">
+          {!props.visitedUser.imageURL ? "EDIT PROFILE" : null}
+        </a>
+        <br />
+        <a href="/books/add" class="btn btn-info">
+          {!props.visitedUser.imageURL ? "ADD BOOK" : null}
+        </a>
+      </div>
 
-      <div id="booksRequestedFromUsers">
-      <p>{props.booksOwnedbyTheUser && props.booksOwnedbyTheUser.length >= 1 ? "Do you want to lend this book?" : null}</p>
+      <p>
+        {props.messagesUnseen && props.messagesUnseen.length >= 1
+          ? "The following books have been approved for borrowing"
+          : null}
+      </p>
+      <div className="user-library container">
+        {props.messagesUnseen
+          ? props.messagesUnseen.map((message, i) => {
+              return (
+                <li key={i}>
+                  {message.content}
+                  <a href={`/private/profile/message/${message._id}/${i}`}>
+                    I got it
+                  </a>
+                </li>
+              );
+            })
+          : null}
+      </div>
+
+      <p>
+        {props.booksOwnedbyTheUser && props.booksOwnedbyTheUser.length >= 1
+          ? "Open requests from other users:"
+          : null}
+      </p>
+      <div className="user-library container">
         <div>
           {props.booksOwnedbyTheUser
             ? props.booksOwnedbyTheUser.map((book, i) => {
                 return (
-                  <p key={i}>
+                  <div key={i}>
                     <img src={book.imageURL} className="image-cover" />
-                    {book.title} {book.author}
-                    <label>Do I want to lend this book:</label>
+                    <div>
+                      {book.title}
+                      <br /> {book.author}
+                    </div>
+                    <p>Do you want to lend this book?</p>
                     <form
                       action={`/private/profile/${props.session.username}/${book._id}`}
                       method="POST"
@@ -63,13 +102,14 @@ function Profile(props) {
                         name="statusBorrowed"
                         value="yes"
                       />
-                      <label for="yes">Yes
-                      <input
-                        type="radio"
-                        id="no"
-                        name="statusBorrowed"
-                        value="no"
-                      />
+                      <label for="yes">
+                        Yes
+                        <input
+                          type="radio"
+                          id="no"
+                          name="statusBorrowed"
+                          value="no"
+                        />
                       </label>
                       <label for="no">No</label>
                       <br />
@@ -77,47 +117,46 @@ function Profile(props) {
                         Submit
                       </button>
                     </form>
-                  </p>
+                  </div>
                 );
               })
             : null}
         </div>
+      </div>
 
+      <p>{props.userLibrary ? "Your library" : null}</p>
+      <div className="user-library container">
+        {props.userLibrary.map((book, i) => {
+          return (
+            <div key={i}>
+              <img src={book.imageURL} className="image-cover" />
+              <p>{book.title}</p>
+              <p>{book.author}</p>
+              <a href={`/books/delete/${book._id}`}>
+                <img className="delete-icon" src="/images/recycle-bin.png" />
+                DELETE BOOK
+              </a>
+            </div>
+          );
+        })}
+      </div>
 
-        <div id="userLibrary">
-          <p>{props.userLibrary ? "Your library" : null}</p>
-          {props.userLibrary.map((book, i) => {
-            return (
-              <p key={i}>
-                <img src={book.imageURL} className="image-cover" />
-                <p>{book.title}</p>
-                <p>{book.author}</p>
-                <a href={`/books/delete/${book._id}`}><img className="delete-icon" src="/images/recycle-bin.png"/>DELETE BOOK</a>
-              </p>
-              
-            );
-          })}
-        </div>
-
-        
-
-        <div id="borrowedBooks">
-          <p>{props.borrowedLibrary.length > 1 ? "Your borrowed library" : null}</p>
-          {props.borrowedLibrary.map((book, i) => {
-            return (
-              <p key={i}>
-                <img src={book.imageURL} className="image-cover" />
-                <p>{book.title}</p> 
-                <p>{book.author}</p>
-              </p>
-            );
-          })}
-        </div>
+      <p>
+          {props.borrowedLibrary.length > 1 ? "Your borrowed library" : null}
+        </p>
+      <div className="user-library container">
+        {props.borrowedLibrary.map((book, i) => {
+          return (
+            <div key={i}>
+              <img src={book.imageURL} className="image-cover" />
+              <p>{book.title}</p>
+              <p>{book.author}</p>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
 }
 
 module.exports = Profile;
-
-
